c6d74f6694fd596848858e8c1fd35b370d1505f0
Mistxs <96961160+Mistxs@users.noreply.github.com>
2023-09-19 03:02:34 +0300

    fix nav on mobile

--- main.py
+++ main.py
@@ -12,22 +12,23 @@ app.register_blueprint(fiosplitter)
 app.static_folder = 'static'
 
 # Роуты для вебстраниц (не действий)
-
+from datetime import datetime
+ts = datetime.now()
 @app.route('/')
 def index():
     title = 'Стартовая'
-    return render_template('index.html', title=title, active_page='start')
+    return render_template('index.html', title=title, active_page='start', ts=ts)
 
 @app.route('/vnedrenie/<route>/<page_name>')
 def vnedrenie_page(page_name,route):
     title = f'{page_name}'
-    return render_template(f'vnedrenie/{route}/{page_name}.html', title=title, active_page=page_name, route='vnedrenie')
+    return render_template(f'vnedrenie/{route}/{page_name}.html', title=title, active_page=page_name, route='vnedrenie', ts=ts)
 
 @app.route('/<route>/<page_name>')
 def page_engine(route,page_name):
     title = f'{page_name}'
-    return render_template(f'/{route}/{page_name}.html', title=title, active_page=page_name, route=route)
+    return render_template(f'/{route}/{page_name}.html', title=title, active_page=page_name, route=route, ts=ts)
 
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run(port=3110)
