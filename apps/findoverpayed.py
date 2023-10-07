4f5b62b1fea5da68890848acf3b638dec79a6e9d
Mistxs <96961160+Mistxs@users.noreply.github.com>
2023-10-07 14:21:56 +0300

    Update findoverpayed.py

--- apps/findoverpayed.py
+++ apps/findoverpayed.py
@@ -16,10 +16,22 @@ def checkDocuments(salon, docid):
     data = []
     url = f"https://api.yclients.com/api/v1/company/{salon}/sale/{docid}"
     response = requests.request("GET", url, headers=headers).json()
+    # print(response)
     if response["data"] is not None:
+        tt_services = []
+        services = response["data"]["state"]["items"]
         payments = response["data"]["state"]["payment_transactions"]
+        loyalty = response["data"]["state"]["loyalty_transactions"]
+        sumItems = 0
+        sumPayments = 0
+        sumLoyalty = 0
+        for item in services:
+            sumItems += item["cost_to_pay_total"]
+            tt_services.append(item["id"])
 
         for item in payments:
+            sumPayments += item["amount"]
+
             if item["sale_item_id"] == None:
                 data.append({
                     "transaction_id": item["id"],
@@ -28,6 +40,13 @@ def checkDocuments(salon, docid):
                     "amount": item["amount"]
                 })
                 return True
+
+        for item in loyalty:
+            if item["sale_item_id"] in tt_services and item["type_id"] == 3:
+                sumLoyalty += item["amount"]
+
+        if sumLoyalty + sumPayments != sumItems:
+            return True
 
 def getTransactions(salon, startdate, enddate):
     url = f"https://api.yclients.com/api/v1/transactions/{salon}"
@@ -52,9 +71,5 @@ def getTransactions(salon, startdate, enddate):
                 "amount": item["amount"],
                 "record_id": date_link
             })
+
     return data
-
-
-
-
-
