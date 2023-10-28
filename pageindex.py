e924c5332d6e0d6a7999cf172eb9cd8541a1f089
Mistxs <96961160+Mistxs@users.noreply.github.com>
2023-10-28 14:39:09 +0300

    faqs update

--- pageindex.py
+++ pageindex.py
@@ -22,5 +22,5 @@ notionindex = {
 "kibana" : 'a7f74c8fd4174e4fb123a2f40eaef340',
 "kkm" : 'a50c00833b2747e69a19a81062b400f4',
 "sql" : '764f3ad43b094d77b0ff2f5af11c0dfc',
-"faq": 'af2358d12ed742ecb58775b732de4578'
+"faq": '8f52411ad1e241b98fae9439070c76dd'
 }
