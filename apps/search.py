d06a31881cefca302c8a6abd8aec69eed7e309c2
Mistxs <96961160+Mistxs@users.noreply.github.com>
2023-11-02 00:19:29 +0300

    search update

--- apps/search.py
+++ apps/search.py
@@ -2,61 +2,27 @@ from flask import Blueprint, request, jsonify
 import requests
 import json
 import datetime
-from config import yatoken
+from config import yakey
 
 tsearch = Blueprint('tsearch', __name__)
-
-tokenexpire = '2023-05-27T10:11:47.199365354Z'
-yatoken = yatoken
-
-# Переменная для хранения временного ключа
-temporary_token = None
-
-
-def auth(token):
-    current_time = datetime.datetime.now()
-    print("current: ", current_time)
-    global tokenexpire, temporary_token
-    tokentime = tokenexpire[:-4]  # Убираем последние 4 символа, чтобы удалить дополнительные десятичные знаки
-    print("tokentime: ", tokentime)
-    timeobj = datetime.datetime.strptime(tokentime, "%Y-%m-%dT%H:%M:%S.%f")
-    print(timeobj)
-    headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        }
-    payload = json.dumps({
-            "yandexPassportOauthToken": token
-        })
-    response = requests.post('https://iam.api.cloud.yandex.net/iam/v1/tokens', headers=headers, data=payload).json()
-    tokenexpire = response['expiresAt']
-    temporary_token = response["iamToken"]
-    print("token is off, new token: ",  tokenexpire, temporary_token)
-
-    return temporary_token
-
 
 @tsearch.route('/search', methods=['POST'])
 def search():
     text = request.args.get('text')
-    print(text)
     data = yasearch(text)  # Ваш список словарей
     count = yacount(text)
     return jsonify({'data': data, 'count': count, 'statuses': list(set([item['status'] for item in data]))})
 
 def yasearch(value):
     url = "https://api.tracker.yandex.net/v2/issues/_search?perPage=10000"
-    global temporary_token
     payload = json.dumps({
         "query": value
     })
-    temporary_token = auth(yatoken)
     headers = {
-        'Authorization': f'Bearer {temporary_token}',
+        'Authorization': f'{yakey}',
         'X-Org-ID': '167455',
-        'Content-Type': 'application/json',
-        'Cookie': 'uid=5/0AAGRuOwY2OQF0BMsqAg=='
+        'Content-Type': 'application/json'
     }
-    print(f"headers = {headers}")
     response = requests.request("POST", url, headers=headers, data=payload).json()
     keys = [item["key"] for item in response]
     description = [item["summary"] for item in response]
@@ -64,7 +30,6 @@ def yasearch(value):
     count = len(keys)
     # data = [{"key": key, "summary": description} for key, description in zip(keys, description)]
     data = [{"key": f"<a href='https://tracker.yandex.ru/{key}' target='_blank'>{key}</a>", "summary": description, "status": status} for key, description, status in zip(keys, description, status)]
-    print(count)
 
     return data
 
@@ -74,12 +39,11 @@ def yacount(value):
     payload = json.dumps({
         "query": value
     })
-    temporary_token = auth(yatoken)
+
     headers = {
-        'Authorization': f'Bearer {temporary_token}',
+        'Authorization': f'{yakey}',
         'X-Org-ID': '167455',
-        'Content-Type': 'application/json',
-        'Cookie': 'uid=5/0AAGRuOwY2OQF0BMsqAg=='
+        'Content-Type': 'application/json'
     }
 
     response = requests.request("POST", url, headers=headers, data=payload).json()
--- trash.py
+++ trash.py
@@ -1,24 +1,11 @@
-def crc16_ccitt(data):
-    crc = 0xFFFF
-    for byte in data:
-        crc ^= (byte << 8)
-        for _ in range(8):
-            if crc & 0x8000:
-                crc = (crc << 1) ^ 0x1021
-            else:
-                crc <<= 1
-            crc &= 0xFFFF
-    return crc
+import re
 
-def calculate_rnm_kkt(serial_number, inn, factory_number):
-    data = serial_number.zfill(10) + inn.zfill(12) + factory_number.zfill(20)
-    crc_result = crc16_ccitt(data.encode('cp866'))
-    crc_decimal = str(crc_result).zfill(6)
-    return crc_decimal
+phone_number = input("Введите номер телефона: ")
 
-# Пример использования:
-serial_number = '0000000001'
-inn = '7708274185'
-factory_number = '00000000177044008917'
-rnm_kkt = calculate_rnm_kkt(serial_number, inn, factory_number)
-print(f"РНМ ККТ равен {rnm_kkt}")
+cleaned_number = re.sub(r"\D", "", phone_number)
+
+if cleaned_number.startswith("8"):
+    cleaned_number = "+7" + cleaned_number[1:]
+
+# Вывод правильного формата номера
+print("Правильный номер телефона в формате '+7XXXXXXXXXX':", cleaned_number)
