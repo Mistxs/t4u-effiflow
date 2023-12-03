0e35f87fa0816aaf816df00f7cb600e184e9a460
Anatoliy F <96961160+Mistxs@users.noreply.github.com>
2023-12-03 17:19:50 +0300

    update darktheme

--- trash.py
+++ trash.py
@@ -1,11 +1,27 @@
 import re
 
-phone_number = input("Введите номер телефона: ")
+string = "143095 ГОЛИЦЫНО 53 ПАРКОВАЯ 7"
 
-cleaned_number = re.sub(r"\D", "", phone_number)
+pattern = r'(\d+)\s+(.+?)\s+(\d)\s+(.+)'
 
-if cleaned_number.startswith("8"):
-    cleaned_number = "+7" + cleaned_number[1:]
+try:
 
-# Вывод правильного формата номера
-print("Правильный номер телефона в формате '+7XXXXXXXXXX':", cleaned_number)
+    substr = string.split(" ")
+    print(substr)
+
+    match = re.match(pattern, string)
+
+    if match:
+        def replace(match):
+            if match.group(1)[-1] == match.group(3):
+                return f"{match.group(1)} {match.group(2)} {match.group(4)}"
+            else:
+                return match.group(0)
+        new_string = re.sub(pattern, replace, string)
+        print(new_string)
+    else:
+        raise Exception("Строка не соответствует регулярному выражению")
+
+except Exception as e:
+    print(f"Ошибка - {e}")
+
