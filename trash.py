6a435d33ff4fc17d404001e2e7e4eb54b95b8ed5
Anatoliy F <96961160+Mistxs@users.noreply.github.com>
2023-12-04 19:38:07 +0300

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,27 +1,4 @@
-import re
-
-string = "143095 ГОЛИЦЫНО 53 ПАРКОВАЯ 7"
-
-pattern = r'(\d+)\s+(.+?)\s+(\d)\s+(.+)'
-
-try:
-
-    substr = string.split(" ")
-    print(substr)
-
-    match = re.match(pattern, string)
-
-    if match:
-        def replace(match):
-            if match.group(1)[-1] == match.group(3):
-                return f"{match.group(1)} {match.group(2)} {match.group(4)}"
-            else:
-                return match.group(0)
-        new_string = re.sub(pattern, replace, string)
-        print(new_string)
-    else:
-        raise Exception("Строка не соответствует регулярному выражению")
-
-except Exception as e:
-    print(f"Ошибка - {e}")
-
+from russiannames.parser import NamesParser
+parser = NamesParser()
+print(parser.parse('Нигматуллин Ринат Ахметович'))
+print(parser.parse('Петрова C.Я.'))
